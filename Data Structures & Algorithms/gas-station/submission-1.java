e928a16a7a8e6afbc32cbeabbe3136ee5276693c
Shrey Mishra <91842474+shreymishra13@users.noreply.github.com>
2026-06-13 16:20:14 +0530

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+
+        int remainingGas =0;
+        int n = gas.length;
+        for(int i = 0 ; i< n ; i++){
+            if(gas[i] + remainingGas < cost[i]){
+                continue;
+                // skipping this starting point
+            }
+
+            int curr = i;
+            boolean isPossible = true;
+            for(int k = 0 ; k<n ; k++){
+                remainingGas = remainingGas + gas[curr] - cost[curr];
+                curr++;
+                curr %= n;
+                if(remainingGas < 0){
+                    //not possible for further movement;
+                    remainingGas = 0;
+                    isPossible = false;
+                    break;
+                }
+            }
+            if(isPossible)return i;
+
+        }
+        
+    return -1;
+    }
+}
