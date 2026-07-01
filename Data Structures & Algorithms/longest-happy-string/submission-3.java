091ef2f02726df5fc411e9a8a4336545d7349244
Shrey Mishra <91842474+shreymishra13@users.noreply.github.com>
2026-07-01 13:30:15 +0530

    Add: longest-happy-string - submission-3

---
+++ Data Structures & Algorithms/longest-happy-string/submission-3.java
@@ -0,0 +1,60 @@
+class Node {
+    char currChar;
+    int freq;
+
+    Node(char c, int count) {
+        this.currChar = c;
+        this.freq = count;
+    }
+}
+class Solution {
+    private boolean canPlace(StringBuilder sb , char c){
+        int n = sb.length();
+        return ! (sb.charAt(n-1) == c && sb.charAt(n-2) == c);
+    }
+    public String longestDiverseString(int a, int b, int c) {
+        if (a == 0 && b == 0 && c == 0)
+            return "";
+        PriorityQueue<Node> pq =
+            new PriorityQueue<>((m, n) ->  n.freq - m.freq );
+        if(a != 0)
+        pq.add(new Node('a', a));
+        if(b != 0)
+        pq.add(new Node('b', b));
+        if(c != 0)
+        pq.add(new Node('c', c));
+
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Node currNode = pq.poll();
+            if (sb.length() < 2) {
+                sb.append(currNode.currChar);
+                if (currNode.freq > 1)
+                    pq.add(new Node(currNode.currChar, currNode.freq - 1));
+            } else {
+                if (canPlace(sb, currNode.currChar)) {
+                    sb.append(currNode.currChar);
+                    if (currNode.freq > 1)
+                        pq.add(new Node(currNode.currChar, currNode.freq - 1));
+                } else {
+                    if(pq.isEmpty()){
+                        return sb.toString();
+                    }else{
+                        Node newNode = pq.poll();
+                        sb.append(newNode.currChar);
+                        if (newNode.freq > 1)
+                            pq.add(new Node(newNode.currChar, newNode.freq - 1));
+                        
+                        sb.append(currNode.currChar);
+                        if (currNode.freq > 1)
+                            pq.add(new Node(currNode.currChar, currNode.freq - 1));
+                        
+                        
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
